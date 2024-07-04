28edcef8d12e0005bbfa6b7b1751ab2aaf7fa2cf
Lucas Marques Mendes Lucas <lucasmqmendes@gmail.com>
2024-07-03 23:09:37 -0300

    First class Position

---
+++ src/boardgame/Position.java
@@ -0,0 +1,36 @@
+package boardgame;
+
+public class Position {
+
+	private int row;
+	private int column;
+	
+	public Position() {}
+
+	public Position(int row, int collumn) {
+		this.row = row;
+		this.column = collumn;
+	}
+
+	public int getRow() {
+		return row;
+	}
+
+	public void setRow(int row) {
+		this.row = row;
+	}
+
+	public int getCollumn() {
+		return column;
+	}
+
+	public void setCollumn(int collumn) {
+		this.column = collumn;
+	}
+	
+	@Override
+	public String toString() {
+		return row + "," + column;
+	}
+	
+}
